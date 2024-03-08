68f94bc09199fc52b50fe5208e3a2e2788531610
abg74123 <bisza_i.d@hotmail.com>
2024-03-08 18:50:30 +0700

    update

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -31,36 +31,34 @@
               <RadioButton v-model="gender" inputId="gender2" name="gender" value="หญิง"/>
               <label for="gender2" class="ml-2">หญิง</label>
             </div>
-            <div class="form-group align-items-center">
+            <div class="flex align-items-center">
               <RadioButton v-model="gender" inputId="gender3" name="gender" value="other"/>
               <label for="gender3" class="ml-2">อื่นๆ</label>
             </div>
           </div>
 
-           <div v-if="gender === 'other'" class="flex flex-col flex-grow">
-          <InputText class="input_text" placeholder="โปรดระบุ" v-model="otherGender"
-                     required/>
-        </div>
+          <div v-if="gender === 'other'" class="flex flex-col flex-grow">
+            <InputText class="input_text" placeholder="โปรดระบุ" v-model="otherGender"
+                       required/>
+          </div>
           <!--          Age-->
           <div class="form-group col">
             <label for="txt_age">อายุ </label>
             <InputText class="input_text" placeholder="อายุ" id="txt_age" v-model="age"
                        required/>
           </div>
-          <div class="form-group row">
             <!--          Weight-->
-            <div class="flex flex-col flex-grow">
+            <div class="form-group flex-col">
               <label for="input_weight">น้ำหนัก </label>
               <InputText class="input_text" placeholder="น้ำหนัก" id="txt_weight" v-model="weight"
                          required/>
             </div>
             <!--          Height-->
-            <div class="flex flex-col flex-grow">
+            <div class="form-group flex-col">
               <label for="txt_height">ส่วนสูง </label>
               <InputText class="input_text" placeholder="ส่วนสูง" id="txt_height" v-model="height"
                          required/>
             </div>
-          </div>
           <!--          Service-->
           <p>บริการที่สนใจ</p>
           <div class="form-group flex-wrap">
@@ -77,12 +75,11 @@
             </div>
           </div>
           <!--          Date Time-->
-<!--          <div class="form-group col">-->
-<!--            <label for="txt_date">วันที่, เวลาที่สะดวก <span class="txt_red">*</span></label>-->
-<!--            <Calendar id="txt_date" :minDate="minDate" placeholder="วันที่, เวลาที่สะดวก" v-model="date" showTime-->
-<!--                      hourFormat="24"/>-->
-<!--          </div>-->
-          {{services}}
+          <!--          <div class="form-group col">-->
+          <!--            <label for="txt_date">วันที่, เวลาที่สะดวก <span class="txt_red">*</span></label>-->
+          <!--            <Calendar id="txt_date" :minDate="minDate" placeholder="วันที่, เวลาที่สะดวก" v-model="date" showTime-->
+          <!--                      hourFormat="24"/>-->
+          <!--          </div>-->
           <div class="form-group row">
             <Button class="btn_submit" type="submit" severity="info" @click="register('message')" label="สมัครสมาชิก"/>
           </div>
@@ -95,7 +92,7 @@
 
 <script>
 
-import liff from "@line/liff";
+// import liff from "@line/liff";
 
 export default {
   name: 'RegisterComp',
@@ -109,7 +106,7 @@ export default {
       weight: "",
       height: "",
       services: [],
-      otherGender:"",
+      otherGender: "",
       service_items1: [
         "ดูดไขมันแขน + หัวไหล่",
         "ดูดไขมันท้องบน-ล่าง",
@@ -132,22 +129,22 @@ export default {
       ],
       minDate: new Date(),
       date: new Date(),
-      loading: true
+      loading: false
     }
   },
   mounted() {
-    console.log("mount")
-    // * Config Line Liff
-    liff.init({liffId: "2003492788-kZQp2v5N", withLoginOnExternalBrowser: true}).then(async () => {
-       // ^ Check Line Liff Login ?
-      if (liff.isLoggedIn()) {
-        const profile = await liff.getProfile()
-        console.log("profile => ", profile)
-        this.loading = false
-      } else {
-        liff.login()
-      }
-    })
+    // console.log("mount")
+    // // * Config Line Liff
+    // liff.init({liffId: "2003492788-kZQp2v5N", withLoginOnExternalBrowser: true}).then(async () => {
+    //   // ^ Check Line Liff Login ?
+    //   if (liff.isLoggedIn()) {
+    //     const profile = await liff.getProfile()
+    //     console.log("profile => ", profile)
+    //     this.loading = false
+    //   } else {
+    //     liff.login()
+    //   }
+    // })
   },
   methods: {
     register(type) {
@@ -162,21 +159,21 @@ export default {
         const minutes = ("0" + this.date.getMinutes()).slice(-2)
         const formatDate = `${day} ${month} ${year} ${hours}:${minutes}น.`
         console.log({formatDate})
-        const mapService = this.services.reduce((accumulator,currentLine) => (accumulator + currentLine + '\n'),'')
+        const mapService = this.services.reduce((accumulator, currentLine) => (accumulator + currentLine + '\n'), '')
         const text = `เพศ${this.gender === 'other' ? this.otherGender : this.gender}\n
             อายุ ${this.age} ปี\n
             น้ำหนัก ${this.weight} kg\n
             ส่วนสูง ${this.height} cm\n
             ${mapService}`
-        liff.sendMessages([
-          {
-            type: "text",
-            text
-            // text: `ลงทะเบียนเข้ารับคำปรึกษา\nชื่อ: ${this.name}\nเบอร์โทร: ${this.tel}\nวัน-เวลาที่นัด : ${formatDate}`
-          }
-        ])
+        // liff.sendMessages([
+        //   {
+        //     type: "text",
+        //     text
+        //     // text: `ลงทะเบียนเข้ารับคำปรึกษา\nชื่อ: ${this.name}\nเบอร์โทร: ${this.tel}\nวัน-เวลาที่นัด : ${formatDate}`
+        //   }
+        // ])
       }
-      liff.closeWindow()
+      // liff.closeWindow()
     }
   }
 }
